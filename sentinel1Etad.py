f23edbca63bd0d9e6d0e84ce26be8cd478f37f49
nuno.miranda <nuno.miranda0@gmail.com>
2020-07-25 07:35:00 +0200

    1) Implemenntation of the query by product name 2) Added docstring to the "get_correction" methods 3) Added Sentinel1ProductName class name to support 1)

--- sentinel1Etad.py
+++ sentinel1Etad.py
@@ -63,11 +63,11 @@ class Sentinel1Etad:
 
     def s1_product_list(self):
         """
-        Read the list of S-1 products that have beenn used to compose the ETAD one
+        Read the list of S-1 products that have been used to compose the ETAD one
 
         """
         xp = 'productComponents/inputProductList/inputProduct/productID'
-        dd = self._xpath_to_list(self._annot,xp)
+        dd = self._xpath_to_list(self._annot, xp)
         return dd
 
 
@@ -150,12 +150,17 @@ class Sentinel1Etad:
         if first_time is None: first_time = df.iloc[0].azimuthTimeMin
         if last_time is None: last_time = df.iloc[-1].azimuthTimeMax
 
-        if product_name is not None:
-            product_name = pathlib.Path(product_name)
-            raise NotImplementedError
-
-
         ix0 = (first_time <= df.azimuthTimeMin) &  (last_time >= df.azimuthTimeMax)
+
+        if product_name is not None:
+            #build a reges based on the name to avoid issues with annotation products and CRC
+            product_name  = Sentinel1ProductName(product_name)
+            product_name.to_annotation(value='[AD]')
+            product_name.crc=''
+            filter = product_name.recompose(with_suffix=False)
+            ix0 = ix0 & (self.burst_catalogue.productID.str.contains(filter,regex=True))
+
+
         if swath is not None:
             if not isinstance(swath, list):
                 #hugly methhod to transfrom string into a list_
@@ -165,8 +170,11 @@ class Sentinel1Etad:
         return df.loc[ix0]
 
 
+    def xpath_to_list(self, xpath, dtype=None, namespace={}, parse_time_func=None):
+        return self._xpath_to_list(self._annot, xpath, dtype=dtype, namespace=namespace, parse_time_func=parse_time_func)
+
     @staticmethod
-    def _xpath_to_list(root, xpath, dtype=None, namespace={}, parse_time_func = None):
+    def _xpath_to_list(root, xpath, dtype=None, namespace={}, parse_time_func=None):
 
         ll = [elt.text for elt in root.findall(xpath,namespace) ]
         if parse_time_func is not None:
@@ -178,10 +186,6 @@ class Sentinel1Etad:
             return ll.item(0)
         else :
             return ll
-
-
-
-
 
 
 class Sentinel1EtadSwath():
@@ -203,6 +207,9 @@ class Sentinel1EtadSwath():
         return len(self._grp.groups)
 
     def get_footprint(self, burst_index_list=None):
+        """method to get the footprints of all the bursts as MultiPolygon
+            It calls in the back the get_footprint of the Sentinel1EtadBurst class
+        """
         if burst_index_list is None:
             burst_index_list = [ int(bix_str[5:]) for bix_str in list(self._grp.groups.keys()) ]
 
@@ -220,6 +227,10 @@ class Sentinel1EtadBurst():
 
 
     def get_footprint(self):
+        """method to get the footprint of ghe bursts as shapely.Polygon. It gets the
+        lat / lon / height grid and extract the 4 corners
+        """
+
         lats = self.__get_etad_param('lats', set_auto_mask=True)
         lons = self.__get_etad_param('lons', set_auto_mask=True)
         heights = self.__get_etad_param('height', set_auto_mask=True)
@@ -229,7 +240,7 @@ class Sentinel1EtadBurst():
         etaf_burst_footprint = []
         for corner in corner_list:
             lat_, lon_, h_ = lats[corner], lons[corner], heights[corner]
-            etaf_burst_footprint.append ((lon_, lat_, h_))
+            etaf_burst_footprint.append((lon_, lat_, h_))
         etaf_burst_footprint = Polygon(etaf_burst_footprint)
         return etaf_burst_footprint
 
@@ -253,6 +264,24 @@ class Sentinel1EtadBurst():
 
 
     def get_tropospheric_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         correction = {}
         prm_list = {'x':'troposphericCorrectionRg'}
         correction = {}
@@ -269,6 +298,24 @@ class Sentinel1EtadBurst():
 
 
     def get_ionospheric_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         correction = {}
         prm_list = {'x':'ionosphericCorrectionRg'}
         correction = {}
@@ -283,6 +330,24 @@ class Sentinel1EtadBurst():
         return correction
 
     def get_geodetic_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         prm_list = {'x':'geodeticCorrectionRg', 'y':'geodeticCorrectionAz'}
         correction = {}
         for dim, field in prm_list.items():
@@ -296,6 +361,24 @@ class Sentinel1EtadBurst():
         return correction
 
     def get_bistatic_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+            '''
         prm_list = {'y':'bistaticCorrectionAz'}
         correction = {}
         for dim, field in prm_list.items():
@@ -309,11 +392,28 @@ class Sentinel1EtadBurst():
         return correction
 
     def get_doppler_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         prm_list = {'x':'dopplerRangeShiftRg'}
         correction = {}
         for dim, field in prm_list.items():
             correction[dim] = self.__get_etad_param(field, set_auto_mask=set_auto_mask, transpose=transpose, meter=meter)
-
         unit = 's'
         if meter:
             unit = 'm'
@@ -321,8 +421,25 @@ class Sentinel1EtadBurst():
         correction['name'] = 'Doppler'
         return correction
 
-
     def get_fmrate_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         prm_list = {'y':'fmMismatchCorrectionAz'}
         correction = {}
         for dim, field in prm_list.items():
@@ -336,14 +453,145 @@ class Sentinel1EtadBurst():
         return correction
 
     def get_sum_correction(self, set_auto_mask=False, transpose=True, meter=False):
+        ''' Retrieve the requested correction in range (and azimuth if applicable).
+            Puts the results in a dict.
+
+            Parameters:
+                set_auto_mask : bool
+                    requested for netCDF4 to avoid retrieving a masked array
+                transpose : bool
+                    requested to retrieve the correction in array following the numpy convention for dimensions
+                meter : bool
+                    transform the result in meters
+
+            Returns:
+                correction : dict
+                    x : correction in range
+                    y : correction in azimuth (if applicable)
+                    unit : 'm' or 's'
+                    name : name of the correction
+        '''
         prm_list = {'x' : 'sumOfCorrectionsRg', 'y':'sumOfCorrectionsAz'}
         correction = {}
         for dim, field in prm_list.items():
             correction[dim] = self.__get_etad_param(field, set_auto_mask=set_auto_mask, transpose=transpose, meter=meter)
-
         unit = 's'
         if meter:
             unit = 'm'
         correction['unit'] = unit
         correction['name'] = 'Sum'
         return correction
+
+
+
+class Sentinel1ProductName():
+    """Class to manipulate the filename of Sentinel 1 products"""
+    def __init__(self, product_name):
+
+        self.__product_name = pathlib.Path(product_name)
+        self.suffix = self.__product_name.suffix
+        self.file_name = self.__product_name.stem
+        self._parts = self.file_name.split('_')
+
+        #trick for SLC
+        if len(self._parts) == 10 and 'SLC' in self.ptype :
+            del self._parts[3]
+            self.ptype = 'SLC_'
+
+
+    @property
+    def mission(self):
+        return self._parts[0]
+
+    @mission.setter
+    def mission(self, value):
+        self._parts[0] = value
+
+    @property
+    def mode(self):
+        return self._parts[1]
+
+    @mode.setter
+    def mode(self, value):
+        self._parts[1] = value
+        print (self._parts[1])
+
+    @property
+    def ptype(self):
+        return self._parts[2]
+
+    @ptype.setter
+    def ptype(self, value):
+        self._parts[2] = value
+
+
+    @property
+    def typepol(self):
+        return self._parts[3]
+
+    @typepol.setter
+    def typepol(self, value):
+        self._parts[3] = value
+
+
+    @property
+    def start_time(self):
+        return self._parts[4]
+
+    @start_time.setter
+    def start_time(self, value):
+        self._parts[4] = value
+
+    @property
+    def stop_time(self):
+        return self._parts[5]
+
+    @stop_time.setter
+    def stop_time(self, value):
+        self._parts[5] = value
+
+    @property
+    def orbit(self):
+        return self._parts[6]
+
+    @orbit.setter
+    def orbit(self, value):
+        self._parts[6] = value
+
+    @property
+    def dtid(self):
+        return self._parts[7]
+
+    @dtid.setter
+    def dtid(self, value):
+        self._parts[7] = value
+
+    @property
+    def crc(self):
+        return self._parts[8]
+
+    @crc.setter
+    def crc(self, value):
+        self._parts[8] = value
+
+
+    def is_annotation(self):
+        if self.typepol[1] == 'A':
+            return True
+        else: return False
+
+    def to_annotation(self, value='A'):
+        ll = list(self.typepol)
+        ll[1]=value
+        self.typepol = ''.join(ll)
+
+    def to_standard(self):
+        ll = list(self.typepol)
+        ll[1]='S'
+        self.typepol = ''.join(ll)
+
+    def recompose(self, with_suffix=True):
+        pp = '_'.join(self._parts)
+        if with_suffix:
+            pp += self.suffix
+        return(pp)

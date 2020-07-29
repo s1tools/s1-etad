553a2a3dbd5c07ce013ffc98ddeeab66772713ea
nuno.miranda <nuno.miranda0@gmail.com>
2020-07-29 09:19:17 +0200

    Prelimianry implementation of: - footprint calculator - kml preview - burst ans swath merger

--- sentinel1Etad.py
+++ sentinel1Etad.py
@@ -14,7 +14,8 @@ from dateutil import parser
 
 from shapely.geometry import Polygon, MultiPolygon
 from shapely.geometry import Point
-
+from shapely.ops import cascaded_union
+import simplekml
 
 
 class Sentinel1Etad:
@@ -82,7 +83,16 @@ class Sentinel1Etad:
         dd['unit'] = 'm'
         return dd
 
+    @property
+    def grid_sampling(self):
+        """ Return the grid spacing in s """
 
+        xp_list = { 'x':'.//productInformation/gridSampling/range', 'y':'.//productInformation/gridSampling/azimuth'}
+        dd = {}
+        for tag, xp in xp_list.items():
+            dd[tag] = self._xpath_to_list(self._annot, xp, dtype=np.float)
+        dd['unit'] = 's'
+        return dd
 
     def processing_setting(self):
         """Read the xml file to  identify the corrections performed
@@ -150,7 +160,7 @@ class Sentinel1Etad:
         if first_time is None: first_time = df.iloc[0].azimuthTimeMin
         if last_time is None: last_time = df.iloc[-1].azimuthTimeMax
 
-        ix0 = (first_time <= df.azimuthTimeMin) &  (last_time >= df.azimuthTimeMax)
+        ix0 = (df.azimuthTimeMin >= first_time) & (df.azimuthTimeMax <= last_time)
 
         if product_name is not None:
             #build a reges based on the name to avoid issues with annotation products and CRC
@@ -188,6 +198,72 @@ class Sentinel1Etad:
             return ll
 
 
+    def get_footprint(self, swath_list=None, merge=False):
+        """method to get the footprints of all the bursts as MultiPolygon
+            It calls in the back the get_footprint of the Sentinel1EtadBurst class
+        """
+        if swath_list is None:
+            swath_list = self.swath_list
+            polys=[]
+            for swath_ in swath_list:
+                for poly_ in self.__getitem__(swath_).get_footprint() :
+                    polys.append(poly_)
+
+        return polys
+
+
+    def __swath_merger(self, burst_variable, swath_list=None, burst_index_list=None):
+        if swath_list is None:
+            swath_list = self.swath_list
+
+        sampling = self.grid_sampling
+
+        first_slant_range_time = self.ds.rangeTimeMin
+        last_slant_range_time = self.ds.rangeTimeMax
+        first_azimuth_time = parser.parse(self.ds.azimuthTimeMin)
+        last_azimuth_time = parser.parse(self.ds.azimuthTimeMax)
+
+        num_samples = np.round((last_slant_range_time-first_slant_range_time) / sampling['x']).astype(np.int)+1
+        num_lines =  np.round((last_azimuth_time - first_azimuth_time).total_seconds() / sampling['y']).astype(np.int)+1
+
+        img = np.zeros((num_lines, num_samples))
+
+        for swath_ in  swath_list:
+            dd_ = self[swath_].merge_sum_correction(burst_index_list=burst_index_list)
+            line_ofs = np.round(dd_['first_azimuth_time'] / sampling['y']).astype(np.int)
+            sample_ofs = np.round((dd_['first_slant_range_time']) /  sampling['x']).astype(np.int)
+
+            slice_y = slice(line_ofs, line_ofs + dd_['x'].shape[0])
+            slice_x = slice(sample_ofs, sample_ofs + dd_['x'].shape[1])
+
+            img[slice_y, slice_x ] = dd_['x']
+        return(img)
+
+        # np.round((last_slant_range_time-first_slant_range_time) / sampling['x']).astype(np.int)+1
+
+    _swath_merger = __swath_merger
+
+    def to_kml(self, kml_file):
+        kml = simplekml.Kml()
+
+        #get the footprints
+        burst_ftps = self.get_footprint()
+        kml_dir = kml.newfolder(name=f"Sentinel1 Timing Correction Grid ")
+        for ix, ftp in enumerate(burst_ftps):
+            x, y = ftp.exterior.xy
+            corner= [ (x[i], y[i]) for i in range(len(x)) ]
+
+            pol = kml_dir.newpolygon(name=str(ix))
+            pol.outerboundaryis = corner
+            pol.altitudeMode = 'absolute'
+            pol.tessellate=1
+            pol.polystyle.fill = 0
+            pol.style.linestyle.width = 2
+
+        kml.save(kml_file)
+
+
+
 class Sentinel1EtadSwath():
     def __init__(self, nc_group):
         self._grp = nc_group
@@ -199,7 +275,8 @@ class Sentinel1EtadSwath():
 
     @property
     def burst_list(self):
-        return list(self._grp.groups.keys())
+        burst_list = [int(burst_str[5:]) for burst_str in list(self._grp.groups.keys())]
+        return burst_list
 
 
     @property
@@ -211,24 +288,119 @@ class Sentinel1EtadSwath():
             It calls in the back the get_footprint of the Sentinel1EtadBurst class
         """
         if burst_index_list is None:
-            burst_index_list = [int(bix_str[5:]) for bix_str in list(self._grp.groups.keys())]
+            burst_index_list = self.burst_list
 
         footprints = [self.__getitem__(bix).get_footprint() for bix in burst_index_list]
         return MultiPolygon(footprints)
 
 
+    def merge_sum_correction(self, burst_index_list=None, set_auto_mask=True, transpose=True, meter=False):
+
+        prm_list = {'x' : 'sumOfCorrectionsRg', 'y':'sumOfCorrectionsAz'}
+        dd = {}
+        for dim, field in prm_list.items():
+            dd_ = self.__burst_merger(field, burst_index_list=burst_index_list, \
+                                                                set_auto_mask=set_auto_mask, \
+                                                                transpose=transpose, \
+                                                                meter=meter)
+            dd[dim] = dd_[field]
+
+        unit = 's'
+        if meter: unit = 'm'
+        dd['unit'] = unit
+        dd['lats'] = self.__burst_merger('lats', transpose=transpose, meter=False, set_auto_mask=set_auto_mask)
+        dd['lons'] = self.__burst_merger('lons', transpose=transpose, meter=False, set_auto_mask=set_auto_mask)
+        dd['sampling'] =  dd_['sampling']
+        dd['first_azimuth_time'] =  dd_['first_azimuth_time']
+        dd['first_slant_range_time'] = dd_['first_slant_range_time']
+        #heights = self.__burst_merger__burst_merger('height', transpose=transpose, meter=False, set_auto_mask=set_auto_mask)
+        return dd
+
+
+    def merge_troposhere_correction(self, burst_index_list=None, set_auto_mask=True, transpose=True, meter=False):
+
+        prm_list = {'x':'troposphericCorrectionRg'}
+        dd = {}
+        for dim, field in prm_list.items():
+            dd_ = self.__burst_merger(field, burst_index_list=burst_index_list, \
+                                                                set_auto_mask=set_auto_mask, \
+                                                                transpose=transpose, \
+                                                                meter=meter)
+            dd[dim] = dd_[field]
+
+        unit = 's'
+        if meter: unit = 'm'
+        dd['unit'] = unit
+        dd['lats'] = self.__burst_merger('lats', transpose=transpose, meter=False, set_auto_mask=set_auto_mask)
+        dd['lons'] = self.__burst_merger('lons', transpose=transpose, meter=False, set_auto_mask=set_auto_mask)
+        dd['sampling'] =  dd_['sampling']
+        dd['first_azimuth_time'] =  dd_['first_azimuth_time']
+        dd['first_slant_range_time'] = dd_['first_slant_range_time']
+
+
+    def __burst_merger(self, burst_var, burst_index_list=None, \
+                            azimuthTimeMin=None, azimuthTimeMax=None, \
+                            set_auto_mask=False, transpose=True, meter=False):
+        """Template method to deburst a variables
+            The deburst strategu is  soimple as the latest line is on top of the oldest
+
+            Parameter:
+                burst_var : one of the burst netcdf variables
+        """
+        if burst_index_list is None:
+            burst_index_list = self.burst_list
+
+        #Find what is the extent of the acquistion in azimuth
+        first_burst = self.__getitem__(burst_index_list[0])
+        last_burst = self.__getitem__(burst_index_list[-1])
+
+        if azimuthTimeMin == None:
+            t0 = first_burst._grp['azimuth'][0]
+        else: t0 = azimuthTimeMin
+
+        if azimuthTimeMax == None:
+            t1 = last_burst._grp['azimuth'][-1]
+        else:
+            t1 = azimuthTimeMax
+
+        #azimuth grid sampling
+        dt = first_burst.sampling['y']
+
+        num_lines = np.round((t1-t0) / dt).astype(np.int)+1
+        num_samples = first_burst._grp.dimensions['rangeExtent'].size
+
+        debursted_var = np.zeros((num_lines, num_samples))
+
+        for b, burst_index in enumerate(burst_index_list):
+            burst_ = self.__getitem__(burst_index)
+            assert(dt == burst_.sampling['y']), 'The azimuth sampling is changing long azimuth'
+            assert(first_burst._grp.gridStartRangeTime0 == burst_._grp.gridStartRangeTime0), 'The 2-way range gridStartRangeTime0 is changing long azimuth'
+
+            #get the timing of the burst and convert into line index
+            az_time_, rg_time_ = burst_.get_burst_grid()
+            line_index_ =  np.round( (az_time_ - t0) / dt).astype(np.int)
+
+            var_ = burst_._get_etad_param(burst_var, set_auto_mask=set_auto_mask, transpose=transpose, meter=meter)
+
+            debursted_var [line_index_,:] = var_
+
+        dd={burst_var : debursted_var, \
+            'first_azimuth_time': t0, \
+            'first_slant_range_time' : first_burst._grp.gridStartRangeTime0, \
+            'sampling' : first_burst.sampling}
+
+        return  dd
+
 
 class Sentinel1EtadBurst():
     def __init__(self, nc_group):
         self._grp =  nc_group
-
-
     def get_footprint(self):
         """method to get the footprint of ghe bursts as shapely.Polygon. It gets the
         lat / lon / height grid and extract the 4 corners
         """
 
-        lats = self.__get_etad_param('lats', set_auto_mask=True)
+        lats, lons = self.__get_etad_param('lats', set_auto_mask=True)
         lons = self.__get_etad_param('lons', set_auto_mask=True)
         heights = self.__get_etad_param('height', set_auto_mask=True)
 
@@ -242,6 +414,21 @@ class Sentinel1EtadBurst():
         return etaf_burst_footprint
 
 
+
+    def get_burst_grid(self, burst_index_list=None):
+        """method to get the t, tau grid of  the burst as
+
+        """
+
+        azimuth = self.__get_etad_param('azimuth', set_auto_mask=True)
+        range = self.__get_etad_param('range', set_auto_mask=True)
+        return azimuth, range
+
+    @property
+    def sampling(self):
+        dd={ 'x':self._grp.gridSamplingRange, 'y':self._grp.gridSamplingAzimuth}
+        dd['unit'] = 's'
+        return dd
 
     def __get_etad_param(self, correction, set_auto_mask=False, transpose=True, meter=False):
 
@@ -259,6 +446,13 @@ class Sentinel1EtadBurst():
 
         return field
 
+    _get_etad_param = __get_etad_param
+
+    def get_lat_lon_heigth(self, transpose=True):
+        lats = self.__get_etad_param('lats', transpose=transpose, meter=False,set_auto_mask=True)
+        lons = self.__get_etad_param('lons', transpose=transpose, meter=False,set_auto_mask=True)
+        h = self.__get_etad_param('height', transpose=transpose, meter=False,set_auto_mask=True)
+        return  lats, lons, h
 
     def get_tropospheric_correction(self, set_auto_mask=False, transpose=True, meter=False):
         ''' Retrieve the requested correction in range (and azimuth if applicable).

8a3b10b302edf326f71b60357103efd687d17fec
Antonio Valentino <Antonio.Valentino@esa.int>
2021-07-28 10:32:22 +0200

    Post release commit

--- s1etad/__init__.py
+++ s1etad/__init__.py
@@ -15,7 +15,7 @@ from .product import (
     Sentinel1Etad, Sentinel1EtadSwath, Sentinel1EtadBurst, ECorrectionType,
 )
 
-__version__ = '0.5.2'
+__version__ = '0.5.3.dev0'
 
 
 # register display functions for Jupyter
